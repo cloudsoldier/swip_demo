f170b44ea8dfc5b0feab8a66aa633ecdb2aba01e
cloudsoldier <36954152+cloudsoldier@users.noreply.github.com>
2025-02-12 12:54:43 +0000

    Update variables.tf

--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -19,28 +19,6 @@ variable "tags" {
   }
 }
 
-variable "azure_client_id" {
-  description = "Azure Client ID"
-  type        = string
-}
-
-
-variable "azure_tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "azure_subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "azure_client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-  sensitive   = true
-}
-
 variable "storage_account_name" {
   description = "The name of the Azure Storage Account"
   type        = string
@@ -59,4 +37,3 @@ variable "account_replication_type" {
   type        = string
   default     = "LRS"
 }
-
